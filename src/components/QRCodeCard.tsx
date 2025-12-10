5527688d5c60e32b81a7805d7453ecd37e6a310d
toonchavez8 <desarrollador5@sistemasicong.com>
2025-12-10 11:53:42 -0600

    style(app): finish styling for componenet

--- src/components/QRCodeCard.tsx
+++ src/components/QRCodeCard.tsx
@@ -12,32 +12,23 @@ const QRCodeCard: React.FC<QRCodeCardProps> = ({
 	description = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
 }) => {
 	return (
-		<div className="min-h-screen bg-slate-300 flex items-center justify-center p-6 ">
-			<div className="bg-white rounded-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.15)] max-w-[320px] w-full">
-				{/* Blue top area */}
-				<div className="bg-gradient-to-br from-blue-500 to-blue-600 rounded-t-2xl p-6 flex items-center justify-center">
-					<div className="bg-blue-600/0 rounded-lg w-full max-w-[240px] p-6 flex items-center justify-center">
-						<div className="bg-blue-400 rounded-lg p-4 w-full flex items-center justify-center">
-							<img
-								src={qrCodeSrc}
-								alt="QR code"
-								className="w-40 h-40 object-cover rounded-md"
-							/>
-						</div>
-					</div>
-				</div>
-
-				{/* White content area */}
-				<div className="p-6 text-center">
-					<h1 className="text-slate-900 text-lg md:text-xl font-bold mb-3 leading-tight">
-						{title}
-					</h1>
-					<p className="text-slate-500 text-sm md:text-base leading-relaxed">
-						{description}
-					</p>
-				</div>
+		<article className=" bg-white flex flex-col max-w-[300px] items-center justify-center rounded-[16px] p-3 shadow-xl">
+			{/* Blue top area */}
+			<figure className="rounded-lg overflow-hidden">
+				<img
+					src={qrCodeSrc}
+					alt="QR code"
+					className="w-full object-cover aspect-square aspect"
+				/>
+			</figure>
+			{/* White content area */}
+			<div className="p-5 text-center">
+				<h1 className="text-slate-900 text-lg md:text-xl font-bold mb-3 leading-tight">
+					{title}
+				</h1>
+				<p className="text-slate-500 text-sm  leading-relaxed">{description}</p>
 			</div>
-		</div>
+		</article>
 	);
 };
 
